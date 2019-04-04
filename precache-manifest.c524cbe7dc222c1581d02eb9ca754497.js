9a89b1e4651cb379719785ea163cc602d2239034
Chelsea Long <clong6@gmail.com>
2019-04-03 22:53:16 -0600

    Updates

---
+++ precache-manifest.c524cbe7dc222c1581d02eb9ca754497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "d0c793da5691a09aafd5",
+    "url": "/Clicky-Game/static/js/main.1e46ce31.chunk.js"
+  },
+  {
+    "revision": "2ef675100e557004c9c1",
+    "url": "/Clicky-Game/static/js/2.685002ad.chunk.js"
+  },
+  {
+    "revision": "d0c793da5691a09aafd5",
+    "url": "/Clicky-Game/static/css/main.10ac6dce.chunk.css"
+  },
+  {
+    "revision": "7f86cd952b84be97e4e609fd8a84deb4",
+    "url": "/Clicky-Game/index.html"
+  }
+];
